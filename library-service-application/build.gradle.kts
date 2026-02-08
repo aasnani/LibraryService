3e654b7c3edfed09b15d9864450ecfb1ace51bf5
Armand Asnani <aasnani@umass.edu>
2026-02-08 21:29:30 +0700

    Add Auth Depencies

--- library-service-application/build.gradle.kts
+++ library-service-application/build.gradle.kts
@@ -3,18 +3,20 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":library-core"))
+
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.doc)
-    runtimeOnly(libs.postgresql)
 
+    runtimeOnly(libs.postgresql)
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
 
-    implementation(project(":library-core"))
-
     testImplementation (platform(libs.testcontainers.bom))
     testImplementation (libs.spring.boot.starter.test)
+    testImplementation (libs.spring.security.test)
     testRuntimeOnly    (libs.junit.platform.launcher)
 }
